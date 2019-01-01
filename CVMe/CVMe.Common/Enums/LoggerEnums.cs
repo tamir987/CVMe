cd1930e1bd88d716a6f043682b684ffca11f603e
tamir987 <tamir5021@gmail.com>
2019-01-01 20:47:44 +0200

    Added LoggerHelper And Installed Newtonsoft

---
+++ CVMe/CVMe.Common/Enums/LoggerEnums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVMe.Common.Enums
+{
+    public enum LoggerOption
+    {
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Fatal
+    }
+}
---
+++ CVMe/CVMe.Common/Helpers/LoggerHelper.cs
@@ -0,0 +1,56 @@
+﻿using Castle.Core.Logging;
+using CVMe.Common.Enums;
+using Newtonsoft.Json;
+
+namespace CVMe.Common.Helpers
+{
+    public interface ILoggerHelper
+    {
+        void LogObject(object messageObject, LoggerOption loggerOption);
+    }
+
+    public class LoggerHelper : ILoggerHelper
+    {
+        private readonly ILogger _logger;
+
+        public LoggerHelper(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogObject(object messageObject, LoggerOption loggerOption)
+        {
+            var message = JsonConvert.SerializeObject(
+                messageObject,
+                Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                }
+            );
+
+            switch (loggerOption)
+            {
+                case LoggerOption.Debug:
+                    _logger.Debug(message);
+                    break;
+
+                case LoggerOption.Info:
+                    _logger.Info(message);
+                    break;
+
+                case LoggerOption.Warning:
+                    _logger.Warn(message);
+                    break;
+
+                case LoggerOption.Error:
+                    _logger.Error(message);
+                    break;
+
+                case LoggerOption.Fatal:
+                    _logger.Fatal(message);
+                    break;
+            }
+        }
+    }
+}
